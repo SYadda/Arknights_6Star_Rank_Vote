d60be1c6ab8f44ba530c652cd74347d37fb8c5ca
hang <2275574766@qq.com>
2023-08-30 20:16:52 +0800

    1.0.0公测版

--- backend.py
+++ backend.py
@@ -1,10 +1,11 @@
-import random, os
+import random, os, pickle
 from flask import Flask, render_template, request, jsonify, send_from_directory
 from flask_cors import CORS, cross_origin
 app = Flask(__name__, template_folder='templates')
 CORS(app)
 
-lst_name = ['纯烬艾雅法拉', '琳琅诗怀雅', '提丰', '圣约送葬人', '缪尔赛思', '霍尔海雅', '淬羽赫默', '伊内丝', '麒麟X夜刀', '仇白', '重岳', '林', '焰影苇草', '缄默德克萨斯', '斥罪', '伺夜', '白铁', '玛恩纳', '百炼嘉维尔', '鸿雪', '多萝西', '黑键', '归溟幽灵鲨', '艾丽妮', '流明', '号角', '菲亚梅塔', '澄闪', '令', '老鲤', '灵知', '耀骑士临光', '焰尾', '远牙', '琴柳', '假日威龙陈', '水月', '帕拉斯', '卡涅利安', '浊心斯卡蒂', '凯尔希', '歌蕾蒂娅', '异客', '灰烬', '夕', '嵯峨', '空弦', '山', '迷迭香', '泥岩', '瑕光', '史尔特尔', '森蚺', '棘刺', '铃兰', '早露', 'W', '温蒂', '傀影', '风笛', '刻俄柏', '年', '阿', '煌', '莫斯提马', '麦哲伦', '赫拉格', '黑', '陈', '斯卡蒂', '银灰', '塞雷娅', '星熊', '夜莺', '闪灵', '安洁莉娜', '艾雅法拉', '伊芙利特', '推进之王', '能天使']
+dict_name = {'能天使': 0, '推进之王': 1, '伊芙利特': 2, '艾雅法拉': 3, '安洁莉娜': 4, '闪灵': 5, '夜莺': 6, '星熊': 7, '塞雷娅': 8, '银灰': 9, '斯卡蒂': 10, '陈': 11, '黑': 12, '赫拉格': 13, '麦哲伦': 14, '莫斯提马': 15, '煌': 16, '阿': 17, '年': 18, '刻俄柏': 19, '风笛': 20, '傀影': 21, '温蒂': 22, 'W': 23, '早露': 24, '铃兰': 25, '棘刺': 26, '森蚺': 27, '史尔特尔': 28, '瑕光': 29, '泥岩': 30, '迷迭香': 31, '山': 32, '空弦': 33, '嵯峨': 34, '夕': 35, '灰烬': 36, '异客': 37, '歌蕾蒂娅': 38, '凯尔希': 39, '浊心斯卡蒂': 40, '卡涅利安': 41, '帕拉斯': 42, '水月': 43, '假日威龙陈': 44, '琴柳': 45, '远牙': 46, '焰尾': 47, '耀骑士临光': 48, '灵知': 49, '老鲤': 50, '令': 51, '澄闪': 52, '菲亚梅塔': 53, '号角': 54, '流明': 55, '艾丽妮': 56, '归溟幽灵鲨': 57, '黑键': 58, '多萝西': 59, '鸿雪': 60, '百炼嘉维尔': 61, '玛恩纳': 62, '白铁': 63, '伺夜': 64, '斥罪': 65, '缄默德克萨斯': 66, '焰影苇草': 67, '林': 68, '重岳': 69, '仇白': 70, '麒麟X夜刀': 71, '伊内丝': 72, '淬羽赫默': 73, '霍尔海雅': 74, '缪尔赛思': 75, '圣约送葬人': 76, '提丰': 77, '琳琅诗怀雅': 78, '纯烬艾雅法拉': 79}
+lst_name = ['能天使', '推进之王', '伊芙利特', '艾雅法拉', '安洁莉娜', '闪灵', '夜莺', '星熊', '塞雷娅', '银灰', '斯卡蒂', '陈', '黑', '赫拉格', '麦哲伦', '莫斯提马', '煌', '阿', '年', '刻俄柏', '风笛', '傀影', '温蒂', 'W', '早露', '铃兰', '棘刺', '森蚺', '史尔特尔', '瑕光', '泥岩', '迷迭香', '山', '空弦', '嵯峨', '夕', '灰烬', '异客', '歌蕾蒂娅', '凯尔希', '浊心斯卡蒂', '卡涅利安', '帕拉斯', '水月', '假日威龙陈', '琴柳', '远牙', '焰尾', '耀骑士临光', '灵知', '老鲤', '令', '澄闪', '菲亚梅塔', '号角', '流明', '艾丽妮', '归溟幽灵鲨', '黑键', '多萝西', '鸿雪', '百炼嘉维尔', '玛恩纳', '白铁', '伺夜', '斥罪', '缄默德克萨斯', '焰影苇草', '林', '重岳', '仇白', '麒麟X夜刀', '伊内丝', '淬羽赫默', '霍尔海雅', '缪尔赛思', '圣约送葬人', '提丰', '琳琅诗怀雅', '纯烬艾雅法拉']
 len_lst_name_1 = len(lst_name) - 1
 
 
@@ -21,30 +22,36 @@ def new_compare():
 @app.route('/save_score', methods=['POST']) 
 @cross_origin()
 def save_score():
-    with open('list/win_record.txt', 'a', encoding='utf8') as f:
-        f.write(request.args.get('win_name') + '\n')
-    with open('list/lose_record.txt', 'a', encoding='utf8') as f:
-        f.write(request.args.get('lose_name') + '\n')
+    with open('list/win_score.pickle', 'rb') as f:
+        lst_win_score = pickle.load(f)
+    with open('list/lose_score.pickle', 'rb') as f:
+        lst_lose_score = pickle.load(f)
+    
+    lst_win_score[dict_name[request.args.get('win_name')]] += 1
+    lst_lose_score[dict_name[request.args.get('lose_name')]] += 1
+
+    with open('list/win_score.pickle', 'wb') as f:
+        pickle.dump(lst_win_score, f)
+    with open('list/lose_score.pickle', 'wb') as f:
+        pickle.dump(lst_lose_score, f)
+
     return 'success'
 
 
 @app.route('/view_final_order', methods=['GET'])
 @cross_origin()
 def view_final_order():
-    with open('list/win_record.txt', 'r', encoding='utf8') as f:
-        lst_win_record = f.read().strip().split('\n')
-    with open('list/lose_record.txt', 'r', encoding='utf8') as f:
-        lst_lose_record = f.read().strip().split('\n')
+    with open('list/win_score.pickle', 'rb') as f:
+        lst_win_score = pickle.load(f)
+    with open('list/lose_score.pickle', 'rb') as f:
+        lst_lose_score = pickle.load(f)
 
-    dict_count = {_: 0 for _ in lst_name}
+    lst_score = [lst_win_score[_] - lst_lose_score[_] for _ in range(len(lst_win_score))]
+    # lst_score = lst_win_score
+    dict_score = dict(zip(lst_name, lst_score))
 
-    for _ in lst_win_record:
-        dict_count[_] = dict_count[_] + 1
-    for _ in lst_lose_record:
-        dict_count[_] = dict_count[_] - 1
-
-    final_name, final_score = zip(*sorted(dict_count.items(), key=lambda _: -_[1]))
-    return jsonify({'name': final_name, 'score': final_score, 'count': '已收集数据 ' + str(len(lst_win_record)) + ' 条'})
+    final_name, final_score = zip(*sorted(dict_score.items(), key=lambda _: -_[1]))
+    return jsonify({'name': final_name, 'score': final_score, 'count': '已收集数据 ' + str(sum(lst_win_score)) + ' 条'})
 
 @app.route('/page', methods=['GET'])
 @cross_origin()
