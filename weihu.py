067c3f9bddb88c149fca25a29264fb05a7bd67df
hang <2275574766@qq.com>
2023-09-01 08:50:10 +0800

    weihu

--- weihu.py
+++ weihu.py
@@ -6,7 +6,7 @@ CORS(app)
 @app.route('/page', methods=['GET'])
 @cross_origin()
 def page():
-    return '服务器维护中，预计2023.9.1 早上8：00恢复使用，感谢大家支持！'
+    return '服务器维护中，预计2023.9.1 早上10：00恢复使用，感谢大家支持！'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9876)
