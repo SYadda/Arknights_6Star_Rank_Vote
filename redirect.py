fd71b6f2db8ba5c7fa84c4a89c7a05fccc8253a7
hang <2275574766@qq.com>
2023-12-12 20:59:40 +0800

    debug

--- redirect.py
+++ redirect.py
@@ -3,9 +3,6 @@ from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return redirect('https://6starvote.me/')
-
 @app.route('/page')
 def index():
     return redirect('https://6starvote.me/')
--- weihu.py
+++ weihu.py
@@ -6,7 +6,7 @@ CORS(app)
 @app.route('/page', methods=['GET'])
 @cross_origin()
 def page():
-    return '服务器维护中，预计2023.9.4 凌晨0：30恢复使用，感谢大家支持！'
+    return '服务器维护中(不小心丢失了一些数据)，预计2023.12.12 晚上9:30恢复使用，感谢大家支持！'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9876)
