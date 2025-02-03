bd2f7a8a6c2c1fd7686e690102763b5243d55b24
hLdont <49443269+hLdont@users.noreply.github.com>
2025-02-03 23:27:41 +0800

    feat: 添加投票记录数据库配置及模型

--- config.py
+++ config.py
@@ -10,6 +10,12 @@ class Config(object):
     # Operators parameters
     DICT_NAME: dict = operators_6star_id
     DICT_PIC_URL: dict = operators_6star_pic
+    # OPERATORS SQLite
+    OPERATORS_VOTE_RECORDS_DB_URL = "./operators_vote_records.db"
+    OPERATORS_VOTE_RECORDS_DB_MAX_CONNECTION = 32
+    OPERATORS_VOTE_RECORDS_DB_STABLE_TIMEOUT = 300  # second
+    OPERATORS_VOTE_RECORDS_DB_DUMP_INTERVAL = 10  # min
+    
 
 class ProductionConfig(Config):
     SERVER_ADDRESS = "https://vote.ltsc.vip"
@@ -17,3 +23,5 @@ class ProductionConfig(Config):
 
 class DevelopmentConfig(Config):
     SERVER_ADDRESS = "http://localhost:9876"
+    # class Config:
+    #     DB_URL = "./archive.db"
--- orm.py
+++ orm.py
@@ -1,14 +1,13 @@
 from peewee import *
 from playhouse.pool import PooledSqliteDatabase, PooledCSqliteExtDatabase
 import datetime
+operators_vote_records_db = PooledSqliteDatabase(
+    Config.OPERATORS_VOTE_RECORDS_DB_URL,
+    max_connections=Config.OPERATORS_VOTE_RECORDS_DB_MAX_CONNECTION,
+    stale_timeout=Config.OPERATORS_VOTE_RECORDS_DB_STABLE_TIMEOUT,
+    pragmas={"journal_mode": "wal", "synchronous": "NORMAL"},
+)
 
-# db = SqliteDatabase('archive.db')
-db = PooledSqliteDatabase("archive.db", max_connections=32, stale_timeout=300)
-class BaseModel(Model):
-    class Meta:
-        database = db
-
-class Archive(BaseModel):
     key = CharField(primary_key=True)
     data = TextField(null = True)
     vote_times = IntegerField(null = True)
@@ -17,5 +16,48 @@ class Archive(BaseModel):
     shared_link = CharField(null = True)
     ip = CharField()
 
-db.connect()
-db.create_tables([Archive])
+class OperatorsVoteRecords(Model):
+    operator_id = IntegerField(null=False, primary_key=True)
+    score_win = FloatField(null=False, default=1)
+    score_lose = FloatField(null=False, default=1)
+
+    class Meta:
+        database = operators_vote_records_db
+        db_table = "operators_vote_records"
+
+
+# TODO: 可以用pydantic优化一下
+class MemoryDB(object):
+    def __init__(self, score_win, score_lose, lock_score_win, lock_score_lose):
+        self.score_win: OrderedDict = score_win
+        self.score_lose: OrderedDict = score_lose
+        self.lock_score_win: list = lock_score_win
+        self.lock_score_lose: list = lock_score_lose
+
+
+def DB_Init():
+    # archive_db
+    archive_db.connect()
+    archive_db.create_tables([Archive])
+
+    # operators_vote_records_db
+    operators_vote_records_db.connect()
+    operators_vote_records_db.create_tables([OperatorsVoteRecords], safe=True)
+
+    # TODO: replace to redis, sqlite(memory mode) or any other memory db...
+    # 全局投票数据（内存），反正就几百个浮点数
+    mem_db = MemoryDB(
+        OrderedDict((id, 1) for id in range(operators_id_dict_length)),
+        OrderedDict((id, 1) for id in range(operators_id_dict_length)),
+        [Lock() for _ in range(operators_id_dict_length)],
+        [Lock() for _ in range(operators_id_dict_length)],
+    )
+
+    with operators_vote_records_db.atomic():
+        for i in range(len(Config.DICT_NAME)):
+            OperatorsVoteRecords_i, _ = OperatorsVoteRecords.get_or_create(
+                operator_id=i, defaults={"score_win": 1, "score_lose": 1}
+            )
+            mem_db.score_win[i] = OperatorsVoteRecords_i.score_win
+            mem_db.score_lose[i] = OperatorsVoteRecords_i.score_lose
+    return mem_db
